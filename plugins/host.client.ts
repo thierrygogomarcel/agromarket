c35287bfeaa3e398a0bd56698d70f0715d429025
thierrygogomarcel <thierrygogomarcel@outlook.fr>
2025-02-07 14:05:39 +0000

    Après docker et nvm install

--- plugins/host.client.ts
+++ plugins/host.client.ts
@@ -1,12 +1,15 @@
-import { defineNuxtPlugin } from '#app';
-import { NhostClient } from '@nhost/nhost-js';
+// plugins/nhost.client.ts  
 
-export default defineNuxtPlugin((nuxtApp: { provide: (arg0: string,arg1: NhostClient) => void; }) => {
+import { NhostClient } from '@nhost/nhost-js';
+import { useRuntimeConfig } from 'nuxt/app';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+
   const nhost = new NhostClient({
-    subdomain: import.meta.env.NUXT_PUBLIC_NHOST_SUBDOMAIN,
-    region: import.meta.env.NUXT_PUBLIC_NHOST_REGION,
+    subdomain: config.public.nhostSubdomain,
+    region: config.public.nhostRegion,
   });
 
-  // Ajouter nhost au contexte Nuxt
-  nuxtApp.provide('nhost', nhost);
+  nuxtApp.provide('nhost', nhost); // Inject nhost into the app context
 });
